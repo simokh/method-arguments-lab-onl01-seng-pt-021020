676d35023719b4f0f49369b1938c4419123e9e4d
Simo Khouili <m.simokhouili@gmail.com>
2020-02-11 03:32:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,8 @@
 def introduction(name)
   puts "Hi, my name is #{name}"
 end
-introduction("Josh")
+
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I learning to program in #{language}"
 end
-introduction_with_language("DAN", "Ember.js")
